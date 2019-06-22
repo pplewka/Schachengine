ac76dfb52751deb08e8cbd8689bdf0b49b0017c5
Patrick Plewka <patrickplewka@googlemail.com>
2019-06-22 19:10:09 +0200

    Added first test. changed mvn dependecies

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BoardTest {
+
+    @Test
+    public void testPass(){
+        assertTrue(true);
+    }
 }
