18c62defaccec25c1971f2785233f89001b5f1a4
Patrick Plewka <patrickplewka@googlemail.com>
2019-06-22 19:30:01 +0200

    testfail

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class BoardTest {
 
     @Test
     public void testPass(){
         assertTrue(true);
+    }
+
+    @Test
+    public void testPass2(){
+        fail("");
     }
 }
