2271b267b83ceedccb473932b636d4f8559f9a4e
Matthias Sennikow <matthias.sennikow@gmail.com>
2019-09-23 00:01:19 +0200

    first try with piece square table

--- Evaluation/src/main/java/EvaluationImpl.java
+++ Evaluation/src/main/java/EvaluationImpl.java
@@ -11,6 +11,137 @@ public class EvaluationImpl implements Evaluation {
      */
     private int[] pieceValues = {0, 0, 0, 0, 1, 3, 5, 3, 9, 100};
 
+    private int [] pstPawnsWhite = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            50, 50, 50, 50, 50, 50, 50, 50,
+            10, 10, 20, 30, 30, 20, 10, 10,
+            5,  5, 10, 25, 25, 10,  5,  5,
+            0,  0,  0, 20, 20,  0,  0,  0,
+            5, -5,-10,  0,  0,-10, -5,  5,
+            5, 10, 10,-20,-20, 10, 10,  5,
+            0,  0,  0,  0,  0,  0,  0,  0
+    };
+
+    private int [] pstPawnsBlack = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            -5, -10, -10, 20, 20, -10, -10,  -5,
+            -5, 5,10,  0,  0,10, 5,  -5,
+            0,  0,  0, -20, -20,  0,  0,  0,
+            -5,  -5, -10, -25, -25, -10,  -5,  -5,
+            -10, -10, -20, -30, -30, -20, -10, -10,
+            50, 50, -50, -50, -50, -50, -50, -50,
+            0,  0,  0,  0,  0,  0,  0,  0
+    };
+
+    private int [] pstKnightsWhite = {
+            -50,-40,-30,-30,-30,-30,-40,-50,
+            -40,-20,  0,  0,  0,  0,-20,-40,
+            -30,  0, 10, 15, 15, 10,  0,-30,
+            -30,  5, 15, 20, 20, 15,  5,-30,
+            -30,  0, 15, 20, 20, 15,  0,-30,
+            -30,  5, 10, 15, 15, 10,  5,-30,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -50,-40,-30,-30,-30,-30,-40,-50,
+    };
+
+    private int [] pstKnightsBlack = {
+            50,40,30,30,30,30,40,50,
+            40,20,  0,  -5,  -5,  0, 20, 40,
+            30,  -5, -10, -15, -15, -10,  5,30,
+            30,  0, -15, -20, -20, -15,  0,30,
+            30,  -5, -15, -20, -20, -15,  5,30,
+            30,  0, -10, -15, -15, 10,  0,30,
+            40,20,  0,  0,  0,  0,20,40,
+            50,40,30,30,30,30,40,50,
+    };
+
+    private int [] pstRooksWhite = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            5, 10, 10, 10, 10, 10, 10,  5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            0,  0,  0,  5,  5,  0,  0,  0
+    };
+
+    private int [] pstRooksBlack = {
+            0,  0,  0,  -5,  -5,  0,  0,  0,
+            5,  0,  0,  0,  0,  0,  0, 5,
+            5,  0,  0,  0,  0,  0,  0, 5,
+            5,  0,  0,  0,  0,  0,  0, 5,
+            5,  0,  0,  0,  0,  0,  0, 5,
+            5,  0,  0,  0,  0,  0,  0, 5,
+            -5, -10, -10, -10, -10, -10, -10,  -5,
+            0,  0,  0,  0,  0,  0,  0,  0,
+    };
+
+    private int [] pstQueensWhite = {
+            -20,-10,-10, -5, -5,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5,  5,  5,  5,  0,-10,
+             -5,  0,  5,  5,  5,  5,  0, -5,
+              0,  0,  5,  5,  5,  5,  0, -5,
+            -10,  5,  5,  5,  5,  5,  0,-10,
+            -10,  0,  5,  0,  0,  0,  0,-10,
+            -20,-10,-10, -5, -5,-10,-10,-20
+    };
+
+    private int [] pstQueensBlack = {
+            20,10,10, 5, 5,10,10,20,
+            10,  0,  -5,  0,  0,  0,  0,10,
+            10,  -5,  -5,  -5,  -5,  -5,  0,10,
+             5,  0,  -5,  -5,  -5,  -5,  0, 5,
+              0,  0,  -5,  -5,  -5,  -5,  0, 5,
+            10,  0,  -5,  -5,  -5,  -5,  0,10,
+            10,  0,  0,  0,  0,  0,  0,10,
+            20,10,10, 5, 5,10,10,20
+    };
+
+    private int PieceSquareTablesValues(byte [] board, boolean blacksTurn) {
+        int value = 0;
+        for (int i=0; i<board.length; i++) {
+            if (i > 1) {
+
+                if(!blacksTurn) {
+                    switch (board[i]) {
+                        case Piece.WPAWN :
+                            value += pstPawnsWhite[i];
+                            break;
+                        case Piece.WQUEEN:
+                            value += pstQueensWhite[i];
+                            break;
+                        case Piece.WROOK:
+                            value += pstRooksWhite[i];
+                            break;
+                        case Piece.WKNIGHT:
+                            value += pstKnightsWhite[i];
+                            break;
+
+                    }
+                } else {
+                    switch (board[i]) {
+                        case Piece.BPAWN :
+                            value += pstPawnsBlack[i];
+                            break;
+                        case Piece.BQUEEN:
+                            value += pstQueensBlack[i];
+                            break;
+                        case Piece.BROOK:
+                            value += pstRooksBlack[i];
+                            break;
+                        case Piece.BKNIGHT:
+                            value += pstKnightsBlack[i];
+                            break;
+
+                    }
+                }
+
+            }
+        }
+        return value * 100; //TODO Maybe?
+    }
 
     private EvaluationImpl() {
     }
@@ -28,6 +159,9 @@ public class EvaluationImpl implements Evaluation {
         boolean blacksTurn = toEvaluate.blacksTurn();
 
         int globalValue = material(board, blacksTurn);
+
+        globalValue += PieceSquareTablesValues(board.getBoard(), toEvaluate.blacksTurn());
+
         globalValue = globalValue + repetitionScore(toEvaluate);
 
         return globalValue;
