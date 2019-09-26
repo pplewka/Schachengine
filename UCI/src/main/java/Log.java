705397d161e1bb9cebc58f8232cd9e8dcb7e9733
Patrick Plewka <patrickplewka@googlemail.com>
2019-09-26 20:32:12 +0200

    First version of logging

---
+++ UCI/src/main/java/Log.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class Log {
+    private volatile static Log uniqueInstance;
+    private boolean writeLog;
+    private String logFilePath;
+    private PrintWriter writer;
+    private boolean ignoreNextWrites;
+
+    public Log(String filename, boolean writeLog) {
+        this.writeLog = writeLog;
+        ignoreNextWrites = false;
+        if (!writeLog) {
+            logFilePath = null;
+            writer = null;
+        } else {
+            logFilePath = filename;
+            try {
+                writer = new PrintWriter(logFilePath, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                InfoHandler.sendMessage("Unable to open file: " + logFilePath);
+                writer = null;
+            }
+        }
+    }
+
+    public static void setInstance(Log instance){
+        uniqueInstance = instance;
+    }
+
+    public static Log getInstance() {
+        return uniqueInstance;
+    }
+
+    public synchronized void writeToLog(List<OptionValuePair> options) {
+        for (OptionValuePair pair : options) {
+            writeToLog("Option: " + pair.option + " value " + pair.value);
+        }
+    }
+
+    public synchronized void writeToLog(String text) {
+        if (writer == null) {
+            return;
+        }
+        if (ignoreNextWrites) {
+            return;
+        }
+        writer.println(text);
+    }
+
+    public synchronized void close() {
+        if(writer == null){
+            return;
+        }
+        writer.close();
+    }
+
+    public synchronized boolean isIgnoreNextWrites() {
+        return ignoreNextWrites;
+    }
+
+    public synchronized void setIgnoreNextWrites(boolean ignoreNextWrites) {
+        this.ignoreNextWrites = ignoreNextWrites;
+    }
+}
